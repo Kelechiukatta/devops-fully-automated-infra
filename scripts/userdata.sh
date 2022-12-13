7fc2b5f999d029efb96d1bdc3a7f42a8fe08fe55
kelechi ukatta <kaycekatta@gmail.com>
2022-12-13 16:20:33 +0100

    update4

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -2,14 +2,14 @@
 # Hardware requirements: AWS Linux 2 with mimum t2.micro type instance & port 8080(application port), 9100 (node-exporter port) should be allowed on the security groups
 # setup for the ansible configuration
 sudo yum update –y
-sudo useradd ansadmin
-sudo passwd ansadmin
-sudo echo ansadmin:ansadmin | chpasswd
+sudo useradd anskaycekatta
+sudo passwd anskattah1122
+sudo echo anskattah1122:anskattah1122| chpasswd
 sudo sed -i "s/.*PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sudo service sshd restart
 sudo echo "%wheel  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sudo service sshd restart
-sudo usermod -aG wheel ansadmin
+sudo usermod -aG wheel anskaycekatta
 
 # tomcat installations
 sudo amazon-linux-extras install tomcat8.5 -y
